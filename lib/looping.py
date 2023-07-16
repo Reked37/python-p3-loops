a13a9b7584b5ece33d712c2ca7da0c312af17c63
Derek Truesdale <dtruesdale37@gmail.com>
2023-07-16 13:42:30 -0500

    lab completed

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i=10
+    while i>0:
+        print(i)
+        i-=1
+    return print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return [value**2 for value in int_list]
 
 def fizzbuzz():
-    # code goes here!
-    pass
+   for i in range(1,101):
+    if (i/3).is_integer() and (i/5).is_integer():
+        print("FizzBuzz")
+    elif (i/3).is_integer():
+        print("Fizz")
+    elif (i/5).is_integer():
+        print("Buzz")
+    else :
+        print(i)
